6c46ffbe4d310238d9370eb084009702cec7a832
David Bauer <davidbauer@Davids-MacBook-Pro.local>
2012-11-04 20:22:10 +0100

    Added check if user has more than 3200 tweets
    the reason for this: user_timeline only accesses the 3200 most recent
    tweets. If user has more than that, first one can't be found.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function findTweet() { 
 	findUser();
 	checkUser();
-	getId ();
+	checkTweetsNumber();
 }
 
 
@@ -32,23 +32,37 @@ $(document).ready(function() {
 		else {
 			var created = data.created_at;
 			var name = data.name;
-			var username = data.screen_name;	
-			html += "Checked Twitter API for @" + username + "(" + name + "). This account was created on " + created; // test
+			var username = data.screen_name;
+			var followersNumber = data.followers_count;
+			var tweetsNumber = data.statuses_count;	
+			html += name + " (@" + username + ") joined Twitter on " + created + ". S(he) currently has <i>" + followersNumber + " followers</i> and has published a total number of <i>" + tweetsNumber + " tweets</i>."; // test
 		}
     	$('.twitterapi').html(html); // test
+    	return tweetsNumber; // need that for later to check if tweetsNumber > 3200
+    	return html;
     	
 	});
 });
 }
 
+//TODO check if user has more than 3200 tweets which makes first one inaccessible
+function checkTweetsNumber(tweetsNumber) {
+	if (tweetsNumber > 3200) {
+		html += "Bummer. @" + username + " has published more than 3200 fweets. This means, Twitter can't find the first tweet.";
+	}
+	$('.twitterapi').html(html);
+}
 
-		// thetweet.innerHTML = "Sorry, no such username found. Please try another one." // error msg if twitter returns error
-
-//TODO extract relevant data to get to first tweet of username
 
 //TODO get ID of first tweet
-
-
+$(document).ready(function() {
+	$.getJSON('https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=davidbauer&since_id=1&count=1&callback=?', function(tweetdata) {
+		var tweetId = tweetdata[0].id_str;
+		var tweetText = tweetdata[0].text;
+		html += "Checked Twitter API: Tweet with ID " + tweetId + " says: " + tweetText; // test
+    	$('.twitterapi').html(html); // test
+	});
+});
 
 
 //TODO create oEmbed of first tweet via ID
@@ -67,15 +81,3 @@ $(document).ready(function() {
 
 }
 */
-
-//API testground
-/* $(document).ready(function() {
-	$.getJSON('https://api.twitter.com/1/users/show.json?screen_name=davidbauer&include_entities=true&callback=?', function(data) {
-		var html = "<ul>";	
-	    html += data.created_at;
-		html += "</ul>"
-    	$('.twitterapi').html(html);
-	});
-});
-*/
-
